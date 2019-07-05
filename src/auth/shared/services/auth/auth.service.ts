5f9e52eddb8ba67b9e45643f8f10e8e5034016f2
Reichsgraf <graf.von.gers@gmail.com>
2019-07-05 16:44:17 +0300

    tons of comments in auth.service.ts and cosmetic improvements

--- src/auth/shared/services/auth/auth.service.ts
+++ src/auth/shared/services/auth/auth.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { BehaviorSubject } from 'rxjs';
-
 // @ts-ignore
 import { Blog } from 'blog';
 
 export interface User {
   email: string;
   authenticated: boolean;
-  // id? name?
+  // FIXME: id? name?
 }
 
 @Injectable()
@@ -20,20 +18,25 @@ export class AuthService {
     private http: HttpClient
   ) {}
 
-  // authentication
-  // observable.do_if_non_auth: this.store.set('user', null)
-  // else: const user: User = { email: email, authenticated: true, id...}
-  // and this.store.set('user', user);
-  // another variants? ngOnInit/ngOnDestroy with observable.
+  // TODO: authentication
+  /* observable.do_if_non_auth: this.store.set('user', null)
+  else: const user: User = { email: email, authenticated: true, id...}
+  and this.store.set('user', user);
+  another variants?
+  ngOnInit/ngOnDestroy with observable - not available in Inject.
+  RouteGuards? (--)
+  BehaviorSubject?
+  Interceptors?
+  learn for AuthState from FireBase.*/
 
   // register
   createUser(email: string, password: string) {
     const body = { email, password };
     this.http.get('temp.json')
       .subscribe(res => console.log(res));
-    // add register
-    // replace with api in future: /api/register?
-    // add return-error
+    // TODO: add register
+    // TODO: replace with api in future: /api/register?
+    // TODO: add return Observable<any> and error
   }
 
   // login
@@ -45,9 +48,8 @@ export class AuthService {
             console.log('Find!');
           }
       });
-    // add another verification logic (?)
-    // replace with api in future: /api/login?
-    // add return-error
+    // TODO: add another verification logic (?)/hash-password?
+    // TODO: replace with api in future: /api/login?
+    // TODO: add return Observable<any> and error
   }
-
 }
